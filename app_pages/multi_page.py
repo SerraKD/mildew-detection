a3ac1bb1246e3f015c6c7536192084c6b6a23ac8
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-11-08 09:54:08 +0000

    create code to generate streamlit pages in multi_page.py

---
+++ app_pages/multi_page.py
@@ -0,0 +1,26 @@
+# imports
+import streamlit as st
+
+
+class MultiPage:
+    '''
+    Class to generate multiple Streamlit pages
+    '''
+
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+
+        st.set_page_config(
+            page_title=self.app_name,
+            page_icon="🌿")
+
+    def add_page(self, title, func) -> None:
+        ''' Add title '''
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio('Menu', self.pages,
+                                format_func=lambda page: page['title'])
+        page['function']()
