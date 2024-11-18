8c5d8959b275b2844eab6b5836950a1c535c2f4d
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-11-18 18:43:10 +0000

    Create mildew detector page content, wire page to app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app_pages.multi_page import MultiPage
 from app_pages.page_proj_summary import page_summary_body
 from app_pages.page_proj_hypothesis import page_project_hypothesis_body
 from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
+from app_pages.page_mildew_detector import page_mildew_detector_body
 
 
 app = MultiPage(app_name="Mildew Detection in Cherry Leaves")
@@ -12,6 +13,7 @@ app = MultiPage(app_name="Mildew Detection in Cherry Leaves")
 # Add app pages using .add_page()
 app.add_page("Project Summary", page_summary_body)
 app.add_page("Leaves Visualizer", page_leaves_visualizer_body)
+app.add_page("Powdery Mildew Detector", page_mildew_detector_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
 
 # Run the app
---
+++ app_pages/page_mildew_detector.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import pandas as pd
+from src.data_management import download_dataframe_as_csv
+from src.machine_learning.predictive_analysis import (
+    load_model_and_predict,
+    resize_input_image,
+    plot_predictions_probabilities
+)
+
+
+def page_mildew_detector_body():
+    # mildew detector page
+
+    st.header("Powdery Mildew Detection")
+    st.success(
+        f"**Business Requirement 2**\n"
+        f"* The client is interested in predicting whether a cherry leaf is infected with"
+        f" **powdery mildew** or not.")
+
+    st.write('---')
+
+    st.info(
+        f" Download images of healthy and powdery mildew-infected leaves for the live prediction. \n\n"
+        f"* Images can be downloaded from **[kaggle](https://www.kaggle.com/codeinstitute/cherry-leaves)**"
+    )
+    st.write(
+        f"> **Upload an image of the sample leaf for live predictions. You may select more than one.**"
+    )
+    images_buffer = st.file_uploader(' ',
+                                     accept_multiple_files=True)
+
+    st.write(
+        f"> Click the **Make Prediction** button for prediction results"
+    )
+    predict_button = st.button("Make Prediction")
+
+    if predict_button:
+        make_live_predict(images_buffer)
+
+
+def make_live_predict(images_buffer):
+    # upload images and make live predictions
+
+    if images_buffer is not None:
+        df_report = pd.DataFrame([])
+        for image in images_buffer:
+
+            img_pil = (Image.open(image))
+            st.info(f"Leaf Sample: **{image.name}**")
+            img_array = np.array(img_pil)
+            st.image(
+                img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
+
+            version = 'v1'
+            resized_img = resize_input_image(img=img_pil, version=version)
+            pred_proba, pred_class = load_model_and_predict(
+                resized_img, version=version)
+            plot_predictions_probabilities(pred_proba, pred_class)
+
+            df_report = df_report.append({"Name": image.name, 'Result': pred_class},
+                                         ignore_index=True)
+
+        if not df_report.empty:
+            st.success("Analysis Report")
+            st.table(df_report)
+            st.markdown(download_dataframe_as_csv(
+                df_report), unsafe_allow_html=True)
