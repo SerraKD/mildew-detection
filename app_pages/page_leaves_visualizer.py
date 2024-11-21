9d09f8f1d328d081572b54b31e705a61f57daccf
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-11-21 22:13:49 +0000

    Fix the code for  image montage

--- app_pages/page_leaves_visualizer.py
+++ app_pages/page_leaves_visualizer.py
@@ -35,6 +35,7 @@ def page_leaves_visualizer_body():
         st.image(avg_var_powdery_mildew,
                  caption='Powdery Mildew infected leaf - Average and Variability')
         st.write("---")
+
     # Show the difference between average and variability images
     if st.checkbox("Differences between average healthy and average powdery mildew infected leaves"):
         diff_between_avgs = plt.imread(f"outputs/{version}/avg_diff.png")
@@ -69,38 +70,46 @@ def page_leaves_visualizer_body():
         st.write("---")
 
 
-def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15, 10)):
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
     # create and display image montage
-    
-    sns.set_style("dark")
-    labels = os.listdir(dir_path)
+  sns.set_style("dark")
+  labels = os.listdir(dir_path)
+
+  # subset the class you are interested to display
+  if label_to_display in labels:
 
     # check if your montage space is greater than subset size
-    images_list = os.listdir(dir_path+'/' + label_to_display)
+    images_list = os.listdir(dir_path+'/'+ label_to_display)
     if nrows * ncols < len(images_list):
-        img_idx = random.sample(images_list, nrows * ncols)
+      img_idx = random.sample(images_list, nrows * ncols)
     else:
-        print(
-            f"Decrease nrows or ncols to create your montage. \n"
-            f"There are {len(images_list)} in your subset. "
-            f"You requested a montage with {nrows * ncols} spaces")
-        return
+      print(
+          f"Decrease nrows or ncols to create your montage. \n"
+          f"There are {len(images_list)} in your subset. "
+          f"You requested a montage with {nrows * ncols} spaces")
+      return
+
 
     # create list of axes indices based on nrows and ncols
-    list_rows = range(0, nrows)
-    list_cols = range(0, ncols)
-    plot_idx = list(itertools.product(list_rows, list_cols))
+    list_rows= range(0,nrows)
+    list_cols= range(0,ncols)
+    plot_idx = list(itertools.product(list_rows,list_cols))
+
 
     # create figure and display images
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
-    for x in range(0, nrows*ncols):
-        img = imread(f"{dir_path}/{label_to_display}/{img_idx[x]}")
-        img_shape = img.shape
-        axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
-        axes[plot_idx[x][0], plot_idx[x][1]].set_title(
-            f"Width {img_shape[1]}px x Height {img_shape[0]}px")
-        axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
-        axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
+    fig, axes = plt.subplots(nrows=nrows,ncols=ncols, figsize=figsize)
+    for x in range(0,nrows*ncols):
+      img = imread(dir_path + '/' + label_to_display + '/' + img_idx[x])
+      img_shape = img.shape
+      axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
+      axes[plot_idx[x][0], plot_idx[x][1]].set_title(f"Width {img_shape[1]}px x Height {img_shape[0]}px")
+      axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
+      axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
     plt.tight_layout()
 
     st.pyplot(fig=fig)
+
+
+  else:
+    print("The label you selected doesn't exist.")
+    print(f"The existing options are: {labels}")
